a23bec5f0d16b1534e485ccdc0f798ef35ca17a4
preetsinghmakkar <preetsinghmakkar154@gmail.com>
2024-07-14 12:27:05 +0530

    Updated Verify Page

--- app/dashboard/verify/page.jsx
+++ app/dashboard/verify/page.jsx
@@ -1,136 +1,3 @@
-// "use client";
-
-// import React, { useEffect, useState } from "react";
-// import { useAccount } from "wagmi";
-// import { Reclaim } from "@reclaimprotocol/js-sdk";
-// import QRCode from "react-qr-code";
-// import Options from "./Options";
-
-// const AccountPage = () => {
-//   const { address } = useAccount();
-//   const [url, setUrl] = useState("");
-//   const [proofVerified, setProofVerified] = useState(false);
-//   const [selectedProofValue, setSelectedProofValue] = useState("LinkedIn");
-
-//   //By default LinkedIn APP ID
-//   let APP_ID = "0xcf6215e623818d0c4112a42E6d48B34B5507E19b";
-//   //BY Default LinkedIn Provider ID
-//   let providerId = "03cdff35-6c27-4626-8231-2b298c3538c0";
-//   let AppSecret =
-//     "0x4a8595336029aba9aada516be890c0046fde8cd3cd54e0723246eb0a80afc337";
-
-//   if (selectedProofValue === "Github") {
-//     APP_ID = "0xda8A667447350d646d90aB54DAcb04dB25730E12";
-//     providerId = "c94476a0-8a75-4563-b70a-bf6124d7c59b";
-//     AppSecret =
-//       "0x7c07ef05bee71a8ac6de07f6d02140721f504ea4afddaba781089f4701adaf2f";
-//   }
-
-//   const reclaimClient = new Reclaim.ProofRequest(APP_ID);
-
-//   async function generateVerificationRequest() {
-//     await reclaimClient.buildProofRequest(providerId);
-
-//     reclaimClient.setSignature(
-//       await reclaimClient.generateSignature(AppSecret)
-//     );
-
-//     const { requestUrl, statusUrl } =
-//       await reclaimClient.createVerificationRequest();
-
-//     setUrl(requestUrl);
-
-//     await reclaimClient.startSession({
-//       onSuccessCallback: (proof) => {
-//         console.log("Verification success", proof);
-//         const { data } = proof.claimData;
-//         console.log("Data : ", data);
-//         setProofVerified(true);
-//       },
-//       onFailureCallback: (error) => {
-//         console.error("Verification failed", error);
-//       },
-//     });
-//   }
-
-//   const handleValueChange = (value) => {
-//     setSelectedProofValue(value);
-//   };
-
-//   useEffect(() => {
-//     const runGenerateVerificationRequest = async () => {
-//       await generateVerificationRequest();
-//     };
-
-//     runGenerateVerificationRequest();
-//   }, [selectedProofValue]);
-
-//   return (
-//     <>
-//       <div className="min-h-screen bg-gray-50 flex flex-col justify-start items-center p-6">
-//         <div className="bg-white shadow-lg rounded-lg p-8 max-w-md w-full">
-//           <h1 className="text-2xl font-bold text-gray-900 mb-6 text-center">
-//             {proofVerified
-//               ? "Proof Verified Successfully"
-//               : "Verify Your Proof"}
-//           </h1>
-//           <p className="text-gray-600 text-center mb-4 ">
-//             Verify your MetaMask account using Reclaim protocol. It is safe and
-//             secure. Don't worry, we will not access your personal info. It is
-//             totally secure.
-//           </p>
-//           {proofVerified && (
-//             <h2 className="text-green-600 text-center mb-4">
-//               Your proof with Kaggle has been verified!
-//             </h2>
-//           )}
-//           <div className="flex flex-col items-center m-5">
-//             {!url ? (
-//               <button
-//                 onClick={generateVerificationRequest}
-//                 className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded-full mb-4 shadow-md transform transition duration-300 hover:scale-105"
-//               >
-//                 Create Claim QR Code
-//               </button>
-//             ) : (
-//               <div className="mt-4">
-//                 <QRCode value={url} size={200} />
-//                 <p className="text-gray-600 text-center mt-4">
-//                   Scan the QR code with your MetaMask wallet to verify your
-//                   proof.
-//                 </p>
-//               </div>
-//             )}
-//           </div>
-//         </div>
-//       </div>
-
-//       <div className="min-h-screen bg-gray-50 flex flex-col justify-start items-center p-6">
-//         <div className="bg-white shadow-lg rounded-lg p-8 max-w-md w-full">
-//           <div className="flex items-center justify-between mb-6">
-//             <Options onValueChange={handleValueChange} />
-//             <h1 className="flex-1 text-2xl font-bold text-gray-900 text-center">
-//               {proofVerified
-//                 ? "Proof Verified Successfully"
-//                 : "Verify Your Proof"}
-//             </h1>
-//           </div>
-//           <p className="text-gray-600 text-center mb-4 ">
-//             Verify your MetaMask Account using Reclaim protocol. It is safe and
-//             secure. Don't worry, we will not access your personal info. It is
-//             totally secure.
-//           </p>
-//           {/*  */}
-
-//           <div>Selected Value: {selectedProofValue}</div>
-//         </div>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default AccountPage;
-
 "use client";
 
 import React, { useEffect, useState } from "react";
@@ -255,7 +122,24 @@ const AccountPage = () => {
             {!url ? (
               <button
                 onClick={generateVerificationRequest}
-                className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded-full mb-4 shadow-md transform transition duration-300 hover:scale-105"
+                className="bg-blue-600 hover:bg-blue-700 text-white font-bo
+//Githhub Username 
+APP ID : 0x9Bc5b367367A8628850f89A44DAEd3627FAF5086
+APP SECRET : 0xf1a6d95f6c09255fb299156630801a5422ca754339dd6e53a335b2071051facc
+APP PROVIDER : eea88931-af56-471f-bbc0-5df8330e52f8
+
+//ProofifyLinkedin
+APP NAME : ProofifyLinkedin
+APP ID : 0xcf6215e623818d0c4112a42E6d48B34B5507E19b
+APP SECRET : 0x4a8595336029aba9aada516be890c0046fde8cd3cd54e0723246eb0a80afc337
+APP PROVIDER : 03cdff35-6c27-4626-8231-2b298c3538c0
+
+//Twitter 
+APP NAME : PROOFIFY Twitter
+APP ID : 0x0cA3571ccAef39E9D6b4f88549BF14210800c147
+APP SECRET : 0xb7133d1307d21f337c543adb18d90b86e1e22e8e435f7818bff769041d7fd6cb
+APP PROVIDER : e3f6a09c-314f-463d-9ad2-cff849e6f774
+ld py-2 px-6 rounded-full mb-4 shadow-md transform transition duration-300 hover:scale-105"
               >
                 Create Claim QR Code
               </button>
