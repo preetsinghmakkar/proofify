1c2034400d2cc5ab54a88e53a27ed2aa34bd71d9
preetsinghmakkar <preetsinghmakkar154@gmail.com>
2024-07-18 13:37:31 +0530

    Updated Supported Chains

--- constants/walletProvider.tsx
+++ constants/walletProvider.tsx
@@ -3,18 +3,18 @@ import "@rainbow-me/rainbowkit/styles.css";
 import { RainbowKitProvider, lightTheme } from "@rainbow-me/rainbowkit";
 import { WagmiProvider } from "wagmi";
 import {
-  mainnet,
-  polygon,
-  arbitrum,
-  base,
-  sepolia,
-  arbitrumSepolia,
+  // mainnet,
+  // polygon,
+  // arbitrum,
+  // base,
+  // sepolia,
+  // arbitrumSepolia,
   polygonAmoy,
-  baseSepolia,
-  optimism,
-  optimismSepolia,
-  polygonMumbai,
-  celoAlfajores,
+  // baseSepolia,
+  // optimism,
+  // optimismSepolia,
+  // polygonMumbai,
+  // celoAlfajores,
 } from "wagmi/chains";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
@@ -34,56 +34,56 @@ export const config = getDefaultConfig({
   appName: "proofify",
   projectId: "777043d787bf56c2f1f2875436d60ccf",
   chains: [
-    mainnet,
-    polygon,
-    arbitrum,
-    base,
-    sepolia,
-    arbitrumSepolia,
-    polygonMumbai,
-    baseSepolia,
-    optimism,
-    optimismSepolia,
-    celoAlfajores,
+    // mainnet,
+    // polygon,
+    // arbitrum,
+    // base,
+    // sepolia,
+    // arbitrumSepolia,
+    // polygonMumbai,
+    // baseSepolia,
+    // optimism,
+    // optimismSepolia,
+    // celoAlfajores,
     polygonAmoy,
-    localhost,
+    // localhost,
   ],
   transports: {
-    [mainnet.id]: http(
-      "https://eth-mainnet.g.alchemy.com/v2/gQ-Ckxh6jH9-r5fGL4o6xiuIPmUQiBof"
-    ),
-    [sepolia.id]: http(
-      "https://eth-sepolia.g.alchemy.com/v2/gQ-Ckxh6jH9-r5fGL4o6xiuIPmUQiBof"
-    ),
-    [arbitrum.id]: http(
-      "https://arb-mainnet.g.alchemy.com/v2/gQ-Ckxh6jH9-r5fGL4o6xiuIPmUQiBof"
-    ),
-    [arbitrumSepolia.id]: http(
-      "https://arb-sepolia.g.alchemy.com/v2/gQ-Ckxh6jH9-r5fGL4o6xiuIPmUQiBof"
-    ),
-    [polygon.id]: http(
-      "https://polygon-mainnet.g.alchemy.com/v2/gQ-Ckxh6jH9-r5fGL4o6xiuIPmUQiBof"
-    ),
-    [polygonMumbai.id]: http(
-      "https://polygon-mumbai.g.alchemy.com/v2/gQ-Ckxh6jH9-r5fGL4o6xiuIPmUQiBof"
-    ),
+    // [mainnet.id]: http(
+    //   "https://eth-mainnet.g.alchemy.com/v2/gQ-Ckxh6jH9-r5fGL4o6xiuIPmUQiBof"
+    // ),
+    // [sepolia.id]: http(
+    //   "https://eth-sepolia.g.alchemy.com/v2/gQ-Ckxh6jH9-r5fGL4o6xiuIPmUQiBof"
+    // ),
+    // [arbitrum.id]: http(
+    //   "https://arb-mainnet.g.alchemy.com/v2/gQ-Ckxh6jH9-r5fGL4o6xiuIPmUQiBof"
+    // ),
+    // [arbitrumSepolia.id]: http(
+    //   "https://arb-sepolia.g.alchemy.com/v2/gQ-Ckxh6jH9-r5fGL4o6xiuIPmUQiBof"
+    // ),
+    // [polygon.id]: http(
+    //   "https://polygon-mainnet.g.alchemy.com/v2/gQ-Ckxh6jH9-r5fGL4o6xiuIPmUQiBof"
+    // ),
+    // [polygonMumbai.id]: http(
+    //   "https://polygon-mumbai.g.alchemy.com/v2/gQ-Ckxh6jH9-r5fGL4o6xiuIPmUQiBof"
+    // ),
     [polygonAmoy.id]: http(
       "https://polygon-amoy.g.alchemy.com/v2/gQ-Ckxh6jH9-r5fGL4o6xiuIPmUQiBof"
     ),
-    [base.id]: http(
-      "https://base-mainnet.g.alchemy.com/v2/gQ-Ckxh6jH9-r5fGL4o6xiuIPmUQiBof"
-    ),
-    [baseSepolia.id]: http(
-      "https://base-sepolia.g.alchemy.com/v2/gQ-Ckxh6jH9-r5fGL4o6xiuIPmUQiBof"
-    ),
-    [optimism.id]: http(
-      "https://opt-mainnet.g.alchemy.com/v2/gQ-Ckxh6jH9-r5fGL4o6xiuIPmUQiBof"
-    ),
-    [optimismSepolia.id]: http(
-      "https://opt-sepolia.g.alchemy.com/v2/gQ-Ckxh6jH9-r5fGL4o6xiuIPmUQiBof"
-    ),
-    [celoAlfajores.id]: http("https://alfajores-forno.celo-testnet.org"),
-    [localhost.id]: http("http://127.0.0.1:8545"),
+    // [base.id]: http(
+    //   "https://base-mainnet.g.alchemy.com/v2/gQ-Ckxh6jH9-r5fGL4o6xiuIPmUQiBof"
+    // ),
+    // [baseSepolia.id]: http(
+    //   "https://base-sepolia.g.alchemy.com/v2/gQ-Ckxh6jH9-r5fGL4o6xiuIPmUQiBof"
+    // ),
+    // [optimism.id]: http(
+    //   "https://opt-mainnet.g.alchemy.com/v2/gQ-Ckxh6jH9-r5fGL4o6xiuIPmUQiBof"
+    // ),
+    // [optimismSepolia.id]: http(
+    //   "https://opt-sepolia.g.alchemy.com/v2/gQ-Ckxh6jH9-r5fGL4o6xiuIPmUQiBof"
+    // ),
+    // [celoAlfajores.id]: http("https://alfajores-forno.celo-testnet.org"),
+    // [localhost.id]: http("http://127.0.0.1:8545"),
   },
 });
 
